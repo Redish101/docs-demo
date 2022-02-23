7e1f2bd520c6510ac5ab87c885ba3c8bb3e06306
redish101 <jiayunluo@outlook.com>
2022-02-23 19:50:20 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://redish101.github.io',
+  baseUrl: '/docs-demo',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
